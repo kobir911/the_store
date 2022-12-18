0c7b73f334751374c877466ba5f872196955ca2b
Kobi Rozin <kobirozin911@gmail.com>
2022-12-18 14:05:51 +0200

    Part-27-Create-Place-Order-Screen

--- README.md
+++ README.md
@@ -169,3 +169,10 @@
 
      1. create input forms
      2. handler submit
+
+
+27.  Create Place Order Screen
+   
+     1. show cart items, payment and address
+     2. handle place order action 
+     3. create order create api
--- frontend/src/App.js
+++ frontend/src/App.js
@@ -16,6 +16,7 @@ import SignInScreen from './screens/SignInScreen';
 import ShippingAddressScreen from './screens/ShippingAddressScreen';
 import SignUpScreen from './screens/SignUpScreen';
 import PaymentMethodScreen from './screens/PaymentMethodScreen';
+import PlaceOrderScreen from './screens/PlaceOrderScreen';
 
 function App() {
   const { state , dispatch: ctxDispatch } = useContext(Store);
@@ -74,6 +75,7 @@ function App() {
             <Route path="/cart" element={<CartScreen />} />
             <Route path="/signin" element={<SignInScreen />} />
             <Route path="/signup" element={<SignUpScreen />} />
+            <Route path="/placeorder" element={<PlaceOrderScreen />} />
             <Route path="/shipping" element={<ShippingAddressScreen />} />
             <Route path="/payment" element={<PaymentMethodScreen />} />
             <Route path="/" element={<HomeScreen />} />
